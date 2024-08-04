4d5ebd1405d2bb0e7758a6aeada06095ba7adea0
coreyqh <chickson@hmc.edu>
2024-08-04 13:25:27 -0700

    added 6 to 1 mux

--- src/multiplexers.sv
+++ src/multiplexers.sv
@@ -48,3 +48,13 @@ module mux5 #(parameter W) (
     assign out = sel[2] ? in4 : (sel[1] ? (sel[0] ? in3 : in2) : (sel[0] ? in1 : in0));
 
 endmodule
+
+module mux6 #(parameter W) (
+    input  logic [W-1:0] in0, in1, in2, in3, in4, in5
+    input  logic [2:0]   sel,
+    output logic [W-1:0] out
+);
+
+    assign out = sel[2] ? (sel[0] ? in5 : in4) : (sel[1] ? (sel[0] ? in3 : in2) : (sel[0] ? in1 : in0));
+
+endmodule
