4186a519a32290fa76436edb9f705f66b100258a
coreyqh <chickson@hmc.edu>
2024-08-24 23:10:40 -0700

    removed unused muxes, added 7 input mux for result mux in datapath

--- src/multiplexers.sv
+++ src/multiplexers.sv
@@ -29,32 +29,24 @@ module mux3 #(parameter W) (
 
 endmodule
 
-module mux4 #(parameter W) (
-    input  logic [W-1:0] in0, in1, in2, in3,
-    input  logic [1:0]   sel,
-    output logic [W-1:0] out
-);
-
-    assign out = sel[1] ? (sel[0] ? in3 : in2) : (sel[0] ? in1 : in0);
-
-endmodule
-
-module mux5 #(parameter W) (
-    input  logic [W-1:0] in0, in1, in2, in3, in4,
+module mux7 #(parameter W) (
+    input  logic [W-1:0] in0, in1, in2, in3, in4, in5, in6
     input  logic [2:0]   sel,
     output logic [W-1:0] out
 );
 
-    assign out = sel[2] ? in4 : (sel[1] ? (sel[0] ? in3 : in2) : (sel[0] ? in1 : in0));
+    always_comb begin
+        case (sel)
+            3'b000:  out = in0;
+            3'b001:  out = in1;
+            3'b010:  out = in2;
+            3'b011:  out = in3;
+            3'b100:  out = in4;
+            3'b101:  out = in5;
+            3'b110:  out = in6;
+            default: out = 'X;
 
-endmodule
-
-module mux6 #(parameter W) (
-    input  logic [W-1:0] in0, in1, in2, in3, in4, in5
-    input  logic [2:0]   sel,
-    output logic [W-1:0] out
-);
-
-    assign out = sel[2] ? (sel[0] ? in5 : in4) : (sel[1] ? (sel[0] ? in3 : in2) : (sel[0] ? in1 : in0));
+        endcase
+    end
 
 endmodule
